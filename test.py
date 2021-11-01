43af8f132308e789778557469d38d65118f0d75a
Jason Huang <dreambroke1019@gmail.com>
2021-11-01 14:29:35 +0800

    123

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ url = "https://api.finmindtrade.com/api/v4/data"
 parameter = {
     "dataset": "TaiwanStockPrice",
     "data_id": "2330",
-    "start_date": "2007-01-01",
+    "start_date": "2020-01-01",
     "end_date": "2020-10-31",
     "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAyMS0xMS0wMSAwOTo1OTozOSIsInVzZXJfaWQiOiJscGxkMTEyMiIsImlwIjoiMTQwLjExMC4yMTUuMjE0In0.BPn9QqKH8oX9uuN90C6uXMp7Z1VN5vcdA72FfyBnYBc", # 參考登入，獲取金鑰
 }
@@ -20,7 +20,7 @@ resp = requests.get(url, params=parameter)
 data = resp.json()
 data = pd.DataFrame(data["data"])
 print(data.head())
-plt.plot(data.date,data.open)
+plot(data.date,data.open)
 
 
 import requests
@@ -38,24 +38,3 @@ data = data.json()
 data = pd.DataFrame(data['data'])
 print(data.head())
 
-# 取得股價
-from FinMind.data import DataLoader
-
-dl = DataLoader()
-# 下載台股股價資料
-stock_data = dl.taiwan_stock_daily(
-stock_id='2609', start_date='2018-01-01', end_date='2021-06-26'
-)
-# 下載三大法人資料
-stock_data = dl.feature.add_kline_institutional_investors(
-stock_data
-) 
-# 下載融資券資料
-stock_data = dl.feature.add_kline_margin_purchase_short_sale(
-stock_data
-)
-
-# 繪製k線圖
-from FinMind import plotting
-
-plotting.kline(stock_data)
