815d0f3a5a3540b605b3c9e2661203d4139aa3a9
Jason Huang <dreambroke1019@gmail.com>
2021-11-01 14:57:54 +0800

    candle plot is ok

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ Created on Mon Nov  1 10:12:32 2021
 import requests
 import pandas as pd
 import matplotlib as plt
+import mplfinance as mpf
+ 
 url = "https://api.finmindtrade.com/api/v4/data"
 parameter = {
     "dataset": "TaiwanStockPrice",
@@ -20,7 +22,7 @@ resp = requests.get(url, params=parameter)
 data = resp.json()
 data = pd.DataFrame(data["data"])
 print(data.head())
-plot(data.date,data.open)
+plot(data.open)
 
 
 import requests
@@ -37,4 +39,19 @@ data = requests.get(url, params=parameter)
 data = data.json()
 data = pd.DataFrame(data['data'])
 print(data.head())
+
+
+
+df = pd.DataFrame(data, columns = ['date', 'open','max','min','close','Trading_Volume'],)
+df = df.set_index(pd.DatetimeIndex(df['date']))
+
+df = df.rename(columns={'open': 'Open'})
+df = df.rename(columns={'max': 'High'})
+df = df.rename(columns={'min': 'Low'})
+df = df.rename(columns={'close': 'Close'})
+df = df.rename(columns={'Trading_Volume': 'Volume'})
+
+
+mpf.plot(df,type='candle')
+
 
